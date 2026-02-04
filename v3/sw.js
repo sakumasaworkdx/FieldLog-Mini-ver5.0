6be799aec6d233c5728d05b2f9e542b2e7110ef1
sakumasaworkdx <sakumasaworkdx@gmail.com>
2026-02-04 23:03:23 +0900

    Update sw.js

--- v3/sw.js
+++ v3/sw.js
@@ -1,24 +1,28 @@
-const CACHE_NAME = "fieldlog-v3-cache-v1";
-
-// フォルダ名やドメインを気にせず、その場所にあるファイルをキャッシュする書き方です
+const CACHE_NAME = "fieldlog-v6.5-cache";
 const urlsToCache = [
   "./",
   "index.html",
-  "app.js"
+  "app.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"
 ];
 
 self.addEventListener("install", (e) => {
+  self.skipWaiting();
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
   );
 });
 
 self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
